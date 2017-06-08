256a68d742b429db31ec5ec767b64498a61e6a50
Alvaro Garcia <derecerca@gmail.com>
2017-06-08 08:45:05 +0200

    Save first draft

--- src/trelloboardconverter/core.clj
+++ src/trelloboardconverter/core.clj
@@ -7,10 +7,40 @@
   [file]
   (clojure.java.io/reader file))
 
-(def input
-  (json/read (read-json "resources/trello2.json")))
+(defn
+  cards
+  [input]
+  (get input "cards"))
+
+(defn list-by-name [input name]
+  (first
+    (filter
+      (fn [list] (= (get list "id") name)) (get input "lists"))))
+
+(defn group-by-list [input]
+  (map
+    (fn [[idList cards]]
+      {:name (get (list-by-name input idList) "name") :cards (map #(get % "name") cards)})
+    (group-by #(get % "idList") (cards input))))
+
+(defn
+  pretty-print
+  [grouped-input]
+  (doall (map #(do (println (str (get % :name) ":"))
+                   (println "")
+                   (doall (map (fn [item] (println (str "* " item))) (get % :cards)))
+                   (println ""))
+              grouped-input)))
 
 (defn -main
   "I don't do trelloboardconverter whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (pretty-print (group-by-list (json/read (read-json (first args))))))
+
+;; (filter #(= (get % "idList") "55bf9909d8050ff4a63fc599") (get input "cards"))
+
+;; cards grouped by list
+;; (group-by #(get % "idList") cards)
+
+;; get all 1st level attributes
+;; (map (fn [[a,b]] a) input)
